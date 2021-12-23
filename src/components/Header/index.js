93914834aa7beab8b721c73b553dc97d4d9f2fc8
anthonycc <anthonyccardozo@gmail.com>
2021-12-22 19:07:31 -0500

    signup functionality w/ mailchimp, new social logos

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,7 +3,7 @@
 ////////////////////////
 
 import React from 'react';
-import { Link } from "react-router-dom"
+// import { Link } from "react-router-dom"
 import spectraLogo from '../../assets/logos/spectrasonic_logo_k-192.jpg'
 
 function Header() {
--- src/components/Signup/index.js
+++ src/components/Signup/index.js
@@ -6,7 +6,7 @@ import React from "react";
 
 import fbLogo from "../../assets/logos/fb_logo.png";
 import twitLogo from "../../assets/logos/twit_logo.png";
-import instaLogo from "../../assets/logos/ig_logo.jpg";
+import instaLogo from "../../assets/logos/ig_logo.png";
 
 function signup() {
   return (
@@ -15,38 +15,76 @@ function signup() {
         <div className="card">
           <h4 className="card-header">Join our mailing list!</h4>
           <div className="card-body">
-            <form>
-              <input
-                className="form-input"
-                placeholder="Name"
-                name="username"
-                type="username"
-                id="username"
-              />
+
+            {/* MAILCHIMP FORM ACTION */}
+            <form action="https://xom.us4.list-manage.com/subscribe/post" method="POST">
+                <input type="hidden" name="u" value="4f93ba4dded6177a1e663084f" />
+                <input type="hidden" name="id" value="ad01b68579" />
+
               <input
                 className="form-input"
                 placeholder="Email"
-                name="email"
                 type="email"
-                id="email"
+                name="MERGE0"
+                id="MERGE0"
               />
+
+              <input
+                className="form-input"
+                placeholder="First Name"
+                type="text"
+                name="MERGE1"
+                id="MERGE1"
+              />
+
+              <input
+                className="form-input"
+                placeholder="Last Name"
+                type="text"
+                name="MERGE2"
+                id="MERGE2"
+              />
+
               <button className="btn d-block w-100" type="submit">
                 Submit
               </button>
-
-              <div className="socials">
-                <h4 className="card-header">Stay in the loop!</h4>
-                <a href="https://www.facebook.com/spectrasonic/" rel="noreferrer" target="_blank">
-                  <img src={fbLogo} alt="link to spectrasonic facebook page" />
-                </a>
-                <a href="https://twitter.com/spectrasonic" rel="noreferrer" target="_blank">
-                  <img src={twitLogo} alt="link to spectrasonic twitter page" />
-                </a>
-                <a href="https://instagram.com/spectrasonic" rel="noreferrer" target="_blank">
-                  <img src={instaLogo} alt="link to spectrasonic instagram page" />
-                </a>
-              </div>
             </form>
+
+
+
+            <div className="socials">
+              <h4 className="card-header">Follow us for updates!</h4>
+
+              <a
+                href="https://www.facebook.com/spectrasonic/"
+                className="fbLogo"
+                rel="noreferrer"
+                target="_blank"
+              >
+                <img src={fbLogo} alt="link to spectrasonic facebook page" />
+              </a>
+
+              <a
+                href="https://twitter.com/spectrasonic"
+                className="twLogo"
+                rel="noreferrer"
+                target="_blank"
+              >
+                <img src={twitLogo} alt="link to spectrasonic twitter page" />
+              </a>
+
+              <a
+                href="https://instagram.com/spectrasonic"
+                className="igLogo"
+                rel="noreferrer"
+                target="_blank"
+              >
+                <img
+                  src={instaLogo}
+                  alt="link to spectrasonic instagram page"
+                />
+              </a>
+            </div>
           </div>
         </div>
       </div>
